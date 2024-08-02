df1b217bead8fe3a943c364f00ff11b178744849
문정원 <moonjw25@naver.com>
2024-08-02 10:09:35 +0900

    Initial commit

---
+++ src/test/java/kr/co/iei/YomiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.iei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YomiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
